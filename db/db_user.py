9bfb1195feafbbe53473f2da9d8fd7e95f164d51
Chetan <sonigrachetan44@gmail.com>
2024-06-18 17:18:08 +0530

    Adds router and db interaction for user

---
+++ db/db_user.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from router.schemas import UserBase
+from db.models import DbUser
+from hashing import Hash
+
+
+def create_user(db: Session, request: UserBase):
+    new_user = DbUser(
+        username = request.username,
+        email = request.email,
+        password = Hash.bcrypt(request.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
